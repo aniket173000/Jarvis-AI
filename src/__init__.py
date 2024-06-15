a3ed46f405cc2fc14597e25c330dc28632700098
Aniket Shrivastav <aniket.shrivastav@webengage.com>
2024-06-16 02:37:33 +0530

    added logic for authentication login of user and music checking.

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,13 @@ import speech_recognition as sr
 import openai as ai
 import os
 import webbrowser
+import cv2 as cv
+from playsound import playsound
+
+name = "Rakesh"
+jarvisInfo = "Hi I am Jarvis A.I. I am your personal assistant. I can do many things for you. Please tell me what you " \
+             "want to do."
+PASSWORD = "I love you 3000"
 
 
 def speaking(text):
@@ -15,7 +22,7 @@ def listencommand():
     with sr.Microphone() as source:
         audio = r.listen(source)
         try:
-            query = r.recognize_google(audio, language='en-in')
+            query = r.recognize_google(audio, language="en-in")
             speaking("You said: " + query)
             return query
         except Exception as e:
@@ -37,31 +44,60 @@ def next_word_after_open(sentence):
     return None
 
 
+def clickMyPhoto():
+    cam = cv.VideoCapture(0)
+    result, image = cam.read()
+    if result:
+        cv.imshow("JarvisFrame", image)
+        cv.imwrite("jarvis.png", image)
+        speaking("Image Captured Successfully")
+        cv.waitKey(0)
+        cv.destroyWindow("JarvisFrame")
+    else:
+        speaking("No Image Captured, Please Try Again")
+
 
 if __name__ == '__main__':
-    print("Hi Aniket Sir, I am your Personal Assistant")
-    speaking("Hi Aniket Sir, I am your Personal Assistant")
-    while True:
-        speaking("I am listening you Sir")
-        yourWords = listencommand()
-        print(yourWords)
-        words = yourWords.split()
-        if "shutdown Jarvis" in yourWords:
-            speaking("Bye Aniket Sir, I am Shutting Down")
-            exit()
-        elif "time" in words:
-            time = datetime.datetime.now().strftime("%H:%M")
-            speaking("Aniket Sir, The time is " + time)
+    print("Hi  Sir, I am your Personal Assistant")
+    speaking("Hi Sir, I am your Personal Assistant, tell me your name please")
+    name = listencommand()
+    counter = 3
+    while counter > 0:
+        speaking("Sir, Your name is " + name + "please tell me your password to login")
+        password = listencommand()
+        if password.lower() == PASSWORD.lower():
+            speaking("Hi " + name + " Sir, I am your Personal Assistant")
+            while True:
+                speaking("I am listening you Sir")
+                yourWords = listencommand()
+                print(yourWords)
+                words = yourWords.split()
+                if "shutdown Jarvis".lower() in yourWords.lower():
+                    speaking("Bye Bye " + name + " Sir, I am Shutting Down")
+                    exit()
+                elif "time" in words:
+                    time = datetime.datetime.now().strftime("%H:%M")
+                    speaking(name + " Sir, The time is " + time)
+                elif "about yourself".lower() in yourWords.lower():
+                    speaking(jarvisInfo)
+                elif "open camera".lower() in yourWords.lower():
+                    clickMyPhoto()
+                elif "play my music" in yourWords.lower():
+                    print("playing music")
+                    musicPath = "/Users/aniketshrivastav/Downloads/songPaisa.mp3"
+                    os.system(f"open {musicPath}")
+                elif "open".lower() in yourWords.lower():
+                    site = next_word_after_open(yourWords)
+                    if site is None:
+                        speaking("Sorry Sir, I did not get that")
+                    else:
+                        speaking("Opening {x} in your browser Sir".replace("{x}", site))
+                        webbrowser.open("https://www.{x}.com".replace("{x}", site))
+                else:
+                    speaking(yourWords + " is not a valid command")
         else:
-            site = next_word_after_open(yourWords)
-            if site is None:
-                speaking("Sorry Sir, I did not get that")
-            else:
-                speaking("Opening {x} in your browser Sir".replace("{x}", site))
-                webbrowser.open("https://www.{x}.com".replace("{x}", site))
-
-            print(yourWords)
-
-
-
-
+            counter = counter - 1
+            speaking("Access Denied Sir, Please Try Again")
+            if counter == 0:
+                speaking("Your Maximum Attempts Reached Sir, Please Contact King Aniket the Great!!!")
+                exit()
